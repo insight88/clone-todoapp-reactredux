c93597c4f31442e8f475852ed48cf7dde170a651
insight88 <kpkim1988@gmail.com>
2020-09-05 12:33:47 +0900

    react-redux: connecting store by using provider

--- src/index.js
+++ src/index.js
@@ -1,76 +1,13 @@
-import { createStore } from 'redux';
-const form = document.querySelector('form');
-const input = document.querySelector('input');
-const ul = document.querySelector('ul');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import { Provider } from 'react-redux';
+import store from './store';
 
-const ADD_TODO = 'ADD_TODO';
-const DELETE_TODO = 'DELETE_TODO';
-
-const addToDo = (text) => {
-  return {
-    type: ADD_TODO,
-    text,
-  };
-};
-
-const deleteToDo = (id) => {
-  return {
-    type: DELETE_TODO,
-    id,
-  };
-};
-
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE_TODO:
-      return state.filter((todo) => todo.id !== action.id);
-    // * filter는 array에서 조건을 만족하는 것들로만 구성된 새로운 array를 return
-    default:
-      return state;
-  }
-};
-// * redux에서 data를 수정할 수 있는 것은 단 하나의 reducer or modifier 함수밖에 없다
-// * state를 절대 직접 mutate하지 말 것 (ex.push, splice)
-// * 바뀐 결과는 reducer에서 새 객체를 만들고 return하여야 한다
-
-const store = createStore(reducer);
-
-store.subscribe(() => console.log(''));
-
-const dispatchAddToDo = (text) => {
-  store.dispatch(addToDo(text));
-};
-
-const dispatchDeleteToDo = (e) => {
-  const id = parseInt(e.target.parentNode.id);
-  // ! HTML로부터 객체로 받아오는 모든 정보는 string
-  store.dispatch(deleteToDo(id));
-};
-
-const paintToDos = () => {
-  const toDos = store.getState();
-  ul.innerHTML = '';
-  toDos.forEach((toDo) => {
-    const li = document.createElement('li');
-    const btn = document.createElement('button');
-    btn.innerText = '💥';
-    btn.addEventListener('click', dispatchDeleteToDo);
-    li.id = toDo.id;
-    li.innerText = toDo.text;
-    li.appendChild(btn);
-    ul.appendChild(li);
-  });
-};
-
-store.subscribe(paintToDos);
-
-const onSubmit = (e) => {
-  e.preventDefault();
-  const toDo = input.value;
-  input.value = '';
-  dispatchAddToDo(toDo);
-};
-
-form.addEventListener('submit', onSubmit);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  // * React로 만든 component를 provider안에 넣으면 provider를 통해 redux store에 접근 가능해진다
+  document.getElementById('root')
+);
