d8ad4c374abdeaed6eda9033944f80bc19b50ff0
insight88 <kpkim1988@gmail.com>
2020-09-06 10:26:49 +0900

    createReducer

--- src/store.js
+++ src/store.js
@@ -1,47 +1,30 @@
 import { createStore } from 'redux';
+import { createAction, createReducer } from '@reduxjs/toolkit';
 
-const ADD = 'ADD';
-const DELETE = 'DELETE';
-
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
-
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id: parseInt(id),
-  };
-};
-
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
-    default:
-      return state;
-  }
-};
-// * reducer는 old state, action을 인자로 받아 new state를 return
-
-const store = createStore(reducer);
-// * 상태는 기본적으로 전부 store에서 집중관리, 커다란 JSON의 결정체
-// * 규모가 클 경우 상태를 카테고리별로 분류하여 사용
-
-export const actionCreators = {
-  addToDo,
-  deleteToDo,
-};
-// * store에 존재하는 state는 action을 통해서만 접근하여 수정가능
+const addToDo = createAction('ADD');
+const deleteToDo = createAction('DELETE');
+// * "ADD"라는 type을 가진 action을 생성
 // * action은 기본적으로 아래와 같은 포맷을 갖고 있는 오브젝트
 // * {
 // *   type: "액션의 종류를 한번에 식별할 수 있는 문자열 혹은 심볼",
 // *   payload: "액션의 실행에 필요한 임의의 데이터",
 // * }
+
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteToDo]: (state, action) =>
+    state.filter((toDo) => toDo.id !== action.payload),
+});
+// * createReducer는 push와 같이 state를 mutate하더라도 new state를 return해준다.
+// * filter와 같이 new state를 return 하던지, push와 같이 state를 mutate하던지 상관없다.
+
+const store = createStore(reducer);
+
+export const actionCreators = {
+  addToDo,
+  deleteToDo,
+};
 
 export default store;
